05cab9817204e68f7b172f74b47d90b3a1c3d853
Daniel Cunha <danielcunha54@gmail.com>
2020-06-02 10:19:33 -0300

    Add static files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import chalk from "chalk";
+import path from "path";
 import routes from "./routes";
 
 const app = express();
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(routes);
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${chalk.green(port)}`);
