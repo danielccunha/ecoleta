a0cef0ce127709169936ecc2c5b99713b2ef5777
Daniel Cunha <danielcunha54@gmail.com>
2020-06-02 21:30:22 -0300

    Add morgan logging

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,12 +4,14 @@ import cors from "cors";
 import chalk from "chalk";
 import path from "path";
 import { errors } from "celebrate";
+import morgan from "morgan";
 import routes from "./routes";
 
 const app = express();
 const port = process.env.PORT || 3333;
 
 app.use(cors());
+app.use(morgan(process.env.MORGAN_MODE || "combined"));
 app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
