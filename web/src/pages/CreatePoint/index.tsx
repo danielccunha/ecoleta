41cd17f7c8b95eceb4731a60638ed808223515b8
Daniel Cunha <danielcunha54@gmail.com>
2020-06-03 15:50:16 -0300

    Add maps and loaded items

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+import { Map, TileLayer, Marker } from "react-leaflet";
+
+import api from "../../services/api";
 
 import logo from "../../assets/logo.svg";
 import "./styles.css";
 
+interface Item {
+  id: number;
+  title: string;
+  image: string;
+}
+
 const CreatePoint = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    api.get("items").then(({ data }) => {
+      const parsedItems = data.map((item: Item) => ({
+        ...item,
+        image: `${api.defaults.baseURL}/uploads/${item.image}`,
+      }));
+
+      setItems(parsedItems);
+    });
+  }, []);
+
   return (
     <div id="page-create-point">
       <header>
@@ -52,6 +74,15 @@ const CreatePoint = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
+          <Map center={[-28.2592873, -52.4358913]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-28.2592873, -52.4358913]} />
+          </Map>
+
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">Estado (UF)</label>
@@ -75,48 +106,12 @@ const CreatePoint = () => {
           </legend>
 
           <ul className="items-grid">
-            <li>
-              <img
-                src="http://localhost:3333/uploads/baterias.svg"
-                alt="Teste"
-              />
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li>
-              <img
-                src="http://localhost:3333/uploads/baterias.svg"
-                alt="Teste"
-              />
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li>
-              <img
-                src="http://localhost:3333/uploads/baterias.svg"
-                alt="Teste"
-              />
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li>
-              <img
-                src="http://localhost:3333/uploads/baterias.svg"
-                alt="Teste"
-              />
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li>
-              <img
-                src="http://localhost:3333/uploads/baterias.svg"
-                alt="Teste"
-              />
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li>
-              <img
-                src="http://localhost:3333/uploads/baterias.svg"
-                alt="Teste"
-              />
-              <span>Óleo de Cozinha</span>
-            </li>
+            {items.map((item) => (
+              <li key={item.id}>
+                <img src={item.image} alt={item.title} />
+                <span>{item.title}</span>
+              </li>
+            ))}
           </ul>
         </fieldset>
 
